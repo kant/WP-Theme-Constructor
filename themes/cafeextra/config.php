672873dbe87071573870931ff3b02a6de00aa3fb
Tobias Bielohlawek <tobi@soundcloud.com>
2011-09-21 21:58:58 +0200

    added custom theme and layout

---
+++ themes/cafeextra/config.php
@@ -0,0 +1,241 @@
+<?php 
+/* Save on 2011-09-21 19:31 */ 
+return array (
+  'sidebar' => 'left',
+  'layout' => 
+  array (
+    'header' => '160',
+    'width' => '1012',
+    'sidebar' => '200',
+    'extra' => '120',
+    'fluid' => 
+    array (
+      'flag' => false,
+      'width' => 80,
+      'min-width' => 960,
+      'max-width' => 1280,
+    ),
+    'home' => 'list',
+    'index' => 'default',
+    'page' => 'page',
+    'single' => 'cafeextra_thumb',
+    'archive' => 'default',
+    'date' => 'list',
+    'category' => 'list',
+    'tag' => 'list',
+    'search' => 'list',
+  ),
+  'title' => 
+  array (
+    'pos' => 'left top',
+    'hidden' => true,
+  ),
+  'content' => 
+  array (
+    'date' => true,
+    'links' => 
+    array (
+      'author' => false,
+      'category' => true,
+      'tags' => true,
+      'comments' => false,
+    ),
+    'widget' => 
+    array (
+      'flag' => false,
+      'after' => 1,
+    ),
+    'social' => 
+    array (
+      'twitter' => '0',
+      'facebook' => '0',
+      'delicious' => '0',
+      'reddit' => '0',
+      'vkontakte' => '0',
+      'digg' => '0',
+      'mixx' => '0',
+      'stumbleupon' => '0',
+      'google' => '0',
+      'memori' => '0',
+    ),
+  ),
+  'comments' => 
+  array (
+    'avatar' => 
+    array (
+      'size' => '64',
+      'pos' => 'right',
+    ),
+  ),
+  'footer' => 
+  array (
+    'text' => 'BÜHNE: Café Extra, Schulstraße 17, 64572 Büttelborn, <span class=\\"footer-tel\\">Telefon: 06152 / 17 88 - 45</span>, Telefax: 17 88 - 65<br>
+ANSCHRIFT: Gemeindeverwaltung Büttelborn, Kleinkunstbühne, Rhönstraße 2 b, 64572 Büttelborn | <a  href=\\"http://www.buettelborn.de/\\" target=\\"_blank\\" title=\\"Homepage der Gemeinde Büttelborn\\" class=\\"footer-link\\">buettelborn.de</a>',
+  ),
+  'fonts' => 
+  array (
+    'title' => 
+    array (
+      'family' => 'Arial,Helvetica,sans-serif',
+      'size' => '28',
+      'weight' => '400',
+      'color' => '#333',
+      'transform' => 'uppercase',
+    ),
+    'description' => 
+    array (
+      'family' => 'Arial,Helvetica,sans-serif',
+      'size' => '12',
+      'weight' => '600',
+      'color' => '#777',
+      'transform' => 'uppercase',
+    ),
+    'header' => 
+    array (
+      'family' => 'Arial,Helvetica,sans-serif',
+    ),
+    'content' => 
+    array (
+      'family' => 'Arial,Helvetica,sans-serif',
+    ),
+  ),
+  'menu' => 
+  array (
+    'pos' => 'right bottom',
+    'width' => false,
+    'flag' => true,
+    'home' => false,
+    'rss' => false,
+    'search' => false,
+    'pages' => 
+    array (
+      'depth' => '0',
+      'exclude' => '0',
+    ),
+    'categories' => 
+    array (
+      'depth' => '0',
+      'exclude' => '0',
+      'group' => false,
+      'title' => '',
+    ),
+  ),
+  'slideshow' => 
+  array (
+    'flag' => false,
+    'layout' => 'in',
+    'onpage' => false,
+    'onsingle' => false,
+    'onarchive' => false,
+    'showposts' => 10,
+    'id' => 0,
+    'height' => 200,
+    'advanced' => 
+    array (
+      'play' => false,
+      'effect' => 'slide',
+      'effectTime' => 300,
+      'timeout' => 3000,
+      'thumb' => false,
+    ),
+  ),
+  'design' => 
+  array (
+    'box' => 
+    array (
+      'flag' => true,
+      'radius' => '6',
+    ),
+    'shadow' => 
+    array (
+      'flag' => true,
+      'x' => '0',
+      'y' => '0',
+      'blur' => '3',
+    ),
+  ),
+  'images' => 
+  array (
+    'body' => 
+    array (
+      'src' => 'bg_stripes.png',
+      'pos' => 'left top',
+      'repeat' => 'repeat',
+      'fixed' => false,
+    ),
+    'wrap' => 
+    array (
+      'src' => 'bg_stripes.png',
+      'pos' => 'left top',
+      'repeat' => 'repeat',
+      'fixed' => false,
+    ),
+    'header' => 
+    array (
+      'src' => 'header-bg-1008px.png',
+      'pos' => 'center top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapper' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'sidebar' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'extrabar' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'footer' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapheader' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapcontent' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+    'wrapfooter' => 
+    array (
+      'src' => '',
+      'pos' => 'left top',
+      'repeat' => 'no-repeat',
+    ),
+  ),
+  'opacity' => 'color',
+  'color' => 
+  array (
+    'bg' => '#ffffff',
+    'bg2' => '#fff5c5',
+    'opacity' => '#fff',
+    'title' => '#333',
+    'title2' => '#555',
+    'text' => '#333',
+    'text2' => '#aaa',
+    'border' => '#aaa',
+    'border2' => '#999',
+    'form' => '#fff',
+    'header1' => '#666666',
+    'header2' => '#cf102d',
+    'header3' => '#ff9933',
+  ),
+  'theme-reload' => '0',
+)
+ ?>
